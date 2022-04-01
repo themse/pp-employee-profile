3974f13386d81042e99f850438383736ab7f46a5
Den Polusmyak <den.polusmyak@gmail.com>
2022-04-01 14:50:37 +0300

    upgrade react to v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 
 import App from 'App';
 import 'styles/global.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+
+ReactDOM.createRoot(rootElement).render(<App />);
